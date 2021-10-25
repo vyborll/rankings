5d01916e1f37493e2359430d8161deb386980695
Vyborll Touch <vyborlltouch@icloud.com>
2021-10-25 17:37:55 -0400

    Testing without MDX

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -8,7 +8,7 @@ import { StoreProvider } from 'easy-peasy';
 import store from '@root/store';
 
 export const client = new ApolloClient({
-  uri: 'http://localhost:3000/api/graphql',
+  uri: 'https://nftranks.vercel.app/api/graphql',
   cache: new InMemoryCache(),
 });
 
